e9d9aa6e4859210a01479a01cbc744a894fa9b75
Fabian Da Silva <fabian.da.silva@hotmail.com>
2024-01-10 01:06:48 -0300

    Segunda entrega

--- index.js
+++ index.js
@@ -1,35 +1,26 @@
-let products = [
-  {
-    title: "Escuadra",
-    price: 123.45,
-    thumbnail:
-      "https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png",
-    id: 1,
-    code: "ESC1",
-  },
-  {
-    title: "Calculadora",
-    price: 234.56,
-    thumbnail:
-      "https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-256.png",
-    id: 2,
-    code: "CAL2",
-  },
-  {
-    title: "Globo Terráqueo",
-    price: 345.67,
-    thumbnail:
-      "https://cdn3.iconfinder.com/data/icons/education-209/64/globe-earth-geograhy-planet-school-256.png",
-    id: 3,
-    code: "GLO3",
-  },
-];
+const fs = require("fs").promises;
 
 class ProductManager {
   #products = [];
 
-  addProduct(title, description, price, thumbnail, code, stock) {
-    //Agrego los productos al arreglo de products inicial y al agregarlo debe crearse un id único
+  constructor() {
+    this.path = "./productos.txt";
+  }
+
+  async addProduct(title, description, price, thumbnail, code, stock) {
+    //Verifico que todos los campos sean obligatorios
+    if (!title || !description || !price || !thumbnail || !code || !stock) {
+      throw new Error("Todos los campos son obligatorios");
+    }
+
+    //Verifico que no se repita el campo code
+    const result = this.#products.filter((product) => product.code === code);
+    //Si el campo code se repite, emito un error
+    if (result.length > 1) {
+      throw new Error("El código del producto ya existe");
+    }
+
+    //Agrego los productos al arreglo vacio de products
     this.#products.push({
       title,
       description,
@@ -40,68 +31,145 @@ class ProductManager {
       id: this.#products.length + 1,
     });
 
-    //Todos los campos son obligatorios
-    if (!title || !description || !price || !thumbnail || !code || !stock) {
-      throw new Error("Todos los campos son obligatorios");
-    }
+    //Declaro la direccion del path
+    const path = this.path;
 
-    //Valido que no se repita el campo code
-    const result = this.#products.filter((product) => product.code === code);
-    //Si el campo code se repite, emito un error
-    if (result.length > 1) {
-      throw new Error("El código del producto ya existe");
-    }
+    //Guardo los productos agregados en el archivo productos.txt pero no los sobreescribo
+    await fs.writeFile(
+      path,
+      JSON.stringify(this.#products, null, "\t"),
+      "utf-8"
+    );
   }
 
-  getProducts() {
-    return this.#products;
+  async getProducts() {
+    //Leo el archivo productos.txt
+    const data = await fs.readFile(this.path, "utf-8");
+
+    //Si el archivo está vacio, emito un error
+    if (data.length === 0) {
+      throw new Error("No hay productos cargados");
+    }
+
+    console.log("getProducts: ", data);
+  }
+  async getProductById(id) {
+    //Busco el producto por id en el archivo productos.txt
+    const result = this.#products.filter((product) => product.id === id);
+
+    //Si el producto no existe, emito un error
+    if (result.length === 0) {
+      throw new Error("El producto no existe");
+    }
+
+    //Devuelvo el resultado en formato objeto
+    console.log("getProductById: ", result[0]);
   }
 
-  getProductById(id) {
-    //Busco el producto por id
-    const product = this.#products.find((product) => product.id === id);
-    //Si no existe el producto, emito un error
-    if (!product) {
-      throw new Error("No existe el producto");
+  async updateProduct(id, obj) {
+    //Busco el producto por id en el archivo productos.txt
+    const result = this.#products.filter((product) => product.id === id);
+
+    //Si el producto no existe, emito un error
+    if (result.length === 0) {
+      throw new Error("El producto no existe");
     }
-    return product;
+
+    //Actualizo el producto
+    const product = result[0];
+    product.title = obj.title;
+    product.description = obj.description;
+    product.price = obj.price;
+    product.thumbnail = obj.thumbnail;
+    product.code = obj.code;
+    product.stock = obj.stock;
+
+    //Declaro la direccion del path
+    const path = this.path;
+
+    //Guardo los productos agregados en el archivo productos.txt pero no los sobreescribo
+    await fs.writeFile(
+      path,
+      JSON.stringify(this.#products, null, "\t"),
+      "utf-8"
+    );
+  }
+
+  async deleteProduct(id) {
+    //Busco el producto por id en el archivo productos.txt
+    const result = this.#products.filter((product) => product.id === id);
+
+    //Si el producto no existe, emito un error
+    if (result.length === 0) {
+      throw new Error("El producto no existe");
+    }
+
+    //El resultado es un arreglo, por lo tanto, lo convierto en objeto y lo imprimo por consola
+    console.log("deleteProduct: ", result[0]);
+
+    //Creo un nuevo arreglo con los productos que no coinciden con el id
+    const newProducts = this.#products.filter((product) => product.id !== id);
+
+    //Declaro la direccion del path
+    const path = this.path;
+
+    //Guardos los productos actualizados en el archivo productos.txt pero los sobreescribo
+    await fs.writeFile(
+      path,
+      JSON.stringify(this.#products, null, "\t"),
+      "utf-8"
+    );
+    //Actualizo el arreglo de productos
+    this.#products = newProducts;
+
+    //Imprimo el arreglo de productos actualizado
+    console.log("deleteProduct actualizado: ", this.#products);
+
+    //Guardos los productos actualizados en el archivo productos.txt pero los sobreescribo
+    await fs.writeFile(
+      path,
+      JSON.stringify(this.#products, null, "\t"),
+      "utf-8"
+    );
   }
 }
 
 //Pruebo el método addProduct
 const manager = new ProductManager();
 manager.addProduct(
-  "Escuadra",
-  "Escuadra de 30 cm",
-  123.45,
+  "Casa",
+  "Regla de plástico transparente",
+  89.99,
   "https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png",
-  "ESC1",
-  20
-);
-manager.addProduct(
-  "Calculadora",
-  "Calculadora científica",
-  234.56,
-  "https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-256.png",
-  "CAL2",
-  10
-);
-manager.addProduct(
-  "Globo Terráqueo",
-  "Globo terráqueo de 30 cm",
-  345.67,
-  "https://cdn3.iconfinder.com/data/icons/education-209/64/globe-earth-geograhy-planet-school-256.png",
-  "GLO3",
+  "REG2",
   15
 );
 
-//Pruebo si code se repite
-manager.addProduct("Globo Terráqueo", "Globo terráqueo de 30 cm", 345.67, "https://cdn3.iconfinder.com/data/icons/education-209/64/globe-earth-geograhy-planet-school-256.png", "GLO3", 15);
+manager.addProduct(
+  "Compás",
+  "Compás de metal",
+  199.99,
+  "https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png",
+  "COM1",
+  10
+);
 
-console.log(manager.getProducts());
+//Pruebo el método getProducts
+// manager.getProducts();
 
 //Pruebo el método getProductById
-const product1 = manager.getProductById(1);
-const product10 = manager.getProductById(10);
+// manager.getProductById(1);
 
-console.log(product1, product10);
+//Pruebo el método updateProduct
+// manager.updateProduct(1, {
+//   title: "Regla de plástico transparente",
+//   description: "Regla de plástico transparentes",
+//   price: 89.992,
+//   thumbnail:
+//     "https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png",
+//   code: "REG2s",
+//   stock: 150,
+// });
+
+//Pruebo el método deleteProduct
+manager.deleteProduct(1);
